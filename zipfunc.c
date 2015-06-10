279d53df486f65be23dc4043a58add3c9493f54c
Lohit9 <tlohit@hotmail.com>
2015-06-10 16:27:02 -0400

    code for q3

---
+++ zipfunc.c
@@ -0,0 +1,22 @@
+// implementation of zip function in c pseudo code
+
+int zip(int a[] ; int b[])
+{
+
+   int c[];
+   int n = a.length;
+   for (int i=0;i<=n;i++)
+   {
+   	  c[i] = a[i];
+   	  c[i+1]=b[i];
+
+
+   }
+
+   return c[];
+
+}
+
+// this algorithm is inomplete, will implement and complete it as soon as possible.
+
+
