d58b1bd52b3c19994c3adb72c6601f1db358b129
Lohit <Lohit9@users.noreply.github.com>
2016-02-19 03:25:18 -0500

    added string programs

--- interview_practice/group_anagrams.cpp
+++ interview_practice/group_anagrams.cpp
@@ -6,11 +6,33 @@ should be {"abc","abcd","abdc","abce","acd"}.
 
 #include <iostream>
 #include <string>
-#include <stack>
+#include <algorithm>
 using namespace std;
+
+
+// Note: The program is not complete but the solution is implement a comperator function to first sort the chatacters of
+// the string and then compare the strings.
+// The sort method accepts a comperator parameter which can be used to get the desired result 
+
+struct compare_chars {
+
+  bool operator()(const std::string l, const std::string r) const {
+    string s1 =l;
+    string s2 =r;
+    sort(s1.begin(),s1.end());
+    sort(s1.begin(),s1.end());
+    return s1==s2;
+  }
+};
 
 void group_anagrams(string arr[], int len){
 
+   //sort(arr,arr+len,compare_length());
+   sort(arr,arr+len,compare_chars());
+  for(int i=0 ;i<len ;i++) {
+    cout << arr[i] << " " ;
+  }
+   cout << endl;
 }
 
 
@@ -20,3 +42,18 @@ int main(){
   group_anagrams(input,length);
   return 0;
 }
+
+/*Bonus program
+Problem: Given 2 integers find the digit that is common
+void does_appear(int a, int b){
+  string str1 =to_string(a);
+  string str2=to_string(b);
+  for(int i=0 ;i<str1.length();i++){
+    for(int j=0 ;j< str2.length() ;j++){
+       if (str1[i] == str2[j]){
+         cout << "Index: " << str1[i] << endl;
+       }
+    }
+  }
+}
+*/
---
+++ interview_practice/infix_eval.cpp
@@ -0,0 +1,17 @@
+/*
+Problem: Evaluate infix expressions
+Ex:
+ 2 + 3  => 5
+ 2*3-(6+5)+8 => 3
+*/
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void eval(string exp){
+
+}
+
+int main(){
+  return 0;
+}
